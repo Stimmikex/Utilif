eeb76442d4dce0723cae3dd5b9361f7f4f14b174
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2020-01-20 11:23:02 +0000

    Minor changes to the footer.blade.php
    mostly style changes, trying to get the look and the layout of the footer right

--- site/resources/views/inc/footer.blade.php
+++ site/resources/views/inc/footer.blade.php
@@ -1,11 +1,44 @@
 <div class="footer">
     <div class="footer__upper">
         <div class="footer__upper__left">
-
+            <p>Opening Hours</p>
+            <h2>Store 1</h2>
+            <div>
+                <p>Week days</p>
+                <ul>
+                    <li>11:00 - 19:00</li>
+                    <li>11:00 - 19:00</li>
+                    <li>11:00 - 19:00</li>
+                    <li>11:00 - 21:00</li>
+                    <li>11:00 - 19:00</li>
+                </ul>
+                <p>Weekends</p>
+                <ul>
+                    <li>11:00 - 18:00</li>
+                    <li>12:30 - 18:00</li>
+                </ul>
+            </div>
+            <h2>Store 2</h2>
+            <div>
+                <p>Week days</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <p>Weekends</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
         </div>
         <div class="footer__upper__center">
             <div class="footer__upper__center__left">
                 <p>My Account</p>
+                <img src="img/accountIcon.png" alt="">
             </div>
             <div class="footer__upper__center__center">
                 <p>About</p>
@@ -15,27 +48,33 @@
             </div>
         </div>
         <div class="footer__upper__right">
-            <p>New Letter</p>
+            {{-- <p>New Letter</p>
             <form action="">
                 <input type="text">
                 <textarea name="" id="" cols="30" rows="10"></textarea>
-            </form>
+                <input type="submit">
+            </form> --}}
+            <a href="#">Fyrirspurnir</a>
         </div>
     </div>
     <div class="footer__lower">
         <div class="footer__lower__level1">
             <div>
+                <img src="img/phoneIcon.png" alt="">
                 <p>Phone</p>
             </div>
             <div>
+                <img src="img/chatIcon.png" alt="">
                 <p>Chat</p>
             </div>
             <div>
+                <img src="img/instaIcon.png" alt="">
                 <p>Social</p>
             </div>
         </div>
         <div class="footer__lower__level2">
-            <p>maker banner</p>
+            <p>Styrmir Óli Þorsteinsson</p>
+            <p>styrmir97@gmail.com</p>
         </div>
     </div>
 </div>
